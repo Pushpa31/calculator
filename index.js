a4bc4fd7f7692f21608dcd090450375d6e1eefa8
Pushpapandey <pushpapandey799@gmail.com>
2019-10-02 17:42:03 +0545

    hello

---
+++ index.js
@@ -0,0 +1,16 @@
+function insert(num)
+{
+	document.getElementById("result").value+=num
+}
+
+function solve()
+{
+	let x= document.getElementById("result").value
+	let y= eval(x)
+	document.getElementById("result").value=y
+}
+
+function clr()
+{
+	document.getElementById("result").value= ""
+}
